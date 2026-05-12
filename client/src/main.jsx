12b8e25b5e59a1ead71f9e7729e02cf208cbb3db
CodewithMK180105 <manishkumar180105@gmail.com>
2026-05-12 13:50:09 +0530

    Display workspace

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
-import { ClerkProvider } from '@clerk/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,7 +14,7 @@ if(!PUBLISHABLE_KEY){
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-         <ClerkProvider>
+         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
             <Provider store={store}>
                 <App />
             </Provider>
